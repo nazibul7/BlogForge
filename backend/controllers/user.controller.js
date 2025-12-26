bc0ecb443ae4b27b0d471908caedcd425374f9ca
Sk Nazibul Hossain <sknazibul5986@gmail.com>
2025-12-26 22:44:01 +0530

    removed username from client & backend

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -10,8 +10,8 @@ export const updateUser = async (req, res, next) => {
     if (req.user.id != req.params.userId) {
         return next(errorHandler(403, 'You are not allowed to update user'))
     }
-    const { password, username, email, profilePicture } = req.body
-    if (!username && !password && !email && !profilePicture) {
+    const { password, email, profilePicture } = req.body
+    if (!password && !email && !profilePicture) {
         return next(errorHandler(400, "No valid fields to update"))
     }
     if (password) {
@@ -20,25 +20,11 @@ export const updateUser = async (req, res, next) => {
         }
         req.body.password = bcryptjs.hashSync(req.body.password, 10)
     }
-    if (username) {
-        if (username.length < 7 || username.length > 20) {
-            return next(errorHandler(400, "username must be between 7 and 20 charcters"))
-        }
-        if (username.includes(' ')) {
-            return next(errorHandler(400, "Username cannot contain spaces"))
-        }
-        if (username != username.toLowerCase()) {
-            return next(errorHandler(400, "Username must not conatin uppercase"))
-        }
-        if (!username.match(/^[a-zA-Z0-9]+$/)) {
-            return next(errorHandler(400, "Username only conatin letters and numbers"))
-        }
-    }
+
     try {
         const updatedUser = await User.findByIdAndUpdate(req.params.userId,
             {
                 $set: {
-                    username: req.body.username,
                     email: req.body.email,
                     profilePicture: req.body.profilePicture,
                     password: req.body.password,
@@ -95,18 +81,18 @@ export const getUsers = async (req, res, next) => {
             }
         ).countDocuments()
         res.status(200).json({
-            users,totalUser,lastMonthUsers
+            users, totalUser, lastMonthUsers
         })
     } catch (error) {
         next(error)
     }
 }
 
-export const getUserComment=async(req,res,next)=>{
+export const getUserComment = async (req, res, next) => {
     try {
-        const user=await User.findById(req.params.userId).select('-password')
-        if(!user){
-            return next(errorHandler(404,"User not found"))
+        const user = await User.findById(req.params.userId).select('-password')
+        if (!user) {
+            return next(errorHandler(404, "User not found"))
         }
         res.status(200).json(user)
     } catch (error) {
--- client/src/components/Comment.jsx
+++ client/src/components/Comment.jsx
@@ -51,11 +51,11 @@ const Comment = ({ comment, onLike, onEdit,onDelete }) => {
     return (
         <div className='flex items-center gap-3 mb-2 border-b p-4 dark:border-gray-600 text-sm'>
             <div className='flex-shrink-0'>
-                <img className='w-10 h-10 rounded-full bg-gray-200' src={user.profilePicture} alt={user.username} />
+                <img className='w-10 h-10 rounded-full bg-gray-200' src={user.profilePicture} alt={user.email} />
             </div>
             <div className='flex-1'>
                 <div>
-                    <span className='font-bold mr-1 text-xs truncate'>{user ? `@${user.username}` : 'anonymous user'}</span>
+                    <span className='font-bold mr-1 text-xs truncate'>{user ? `@${user.email}` : 'anonymous user'}</span>
                     <span className='text-gray-500 text-xs'>{moment(comment.createdAt).fromNow()}</span>
                 </div>
                 {isEditing ? (
